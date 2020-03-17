6823fb29c152e3c97f6d61d64ea406fb8447070f
Mircea Cozmei <micozmei@hotmail.com>
2020-03-16 17:03:17 -0700

    Update Final(Rev1InProgress).m

--- Final(Rev1InProgress).m
+++ Final(Rev1InProgress).m
@@ -7,152 +7,144 @@ BW
 
 %% Global Image De-Noising
 function Global
-% Black/White image
-d2 = imread('moon_bw','bmp');
-d2d = double(d2);
-d2t= fft2(d2d); 
+    
+    % Black/White Image
+    bw = imread('moon_bw','bmp');
+    bw_d = double(bw);
+    bw_t = fft2(bw_d); 
 
-width = 0.001; %Filter Width
-kx = 1:size(d2,2); ky = 1:size(d2,1);
-[Kx,Ky] = meshgrid(kx,ky);
-F = exp(-width*((Kx-size(d2,2)/2).^2+(Ky-size(d2,1)/2).^2));
-Ft = fftshift(F);
+    Width = 0.001; %Filter Width
+    kx = 1:size(bw,2); ky = 1:size(bw,1);
+    [Kx,Ky] = meshgrid(kx,ky);
+    F = exp(-Width*((Kx-size(bw,2)/2).^2+(Ky-size(bw,1)/2).^2));
+    Ft = fftshift(F);
 
-% figure(1), surfl(Kx,Ky,F), shading interp; %Filter visualization
-% xlabel('X-dir'), ylabel('Y-Dir'), zlabel('Filter Magnitude');
-% title('Image Layer Filter','fontsize',20), xlim([1, 460]), ylim([1, 288]); %Resolution
+    bw_t_f = Ft.*bw_t;
+    bw_t = ifft2(bw_t_f);
+    bw_f = uint8(abs(bw_t));
 
-d2tf = Ft.*d2t;
-d2t = ifft2(d2tf);
-d2f = uint8(abs(d2t));
+    figure(1), 
+    imshow(bw), title('Noisy Image','fontsize',20);
 
-figure(2), 
-imshow(d2), title('Noisy Image','fontsize',20);
+    figure(2), 
+    imshow(bw_f); title('Filtered Image','fontsize',20);
 
-figure(3), 
-imshow(d2f); title('Filtered Image','fontsize',20);
+    figure(3)
+    pcolor(log(abs(fftshift(bw_t_f))+1)),shading interp,colormap(hot);
+    title('Log Frequency Spectrum','fontsize',20), xlabel('Kx'), ...
+        ylabel('Ky');
 
-figure(4)
-pcolor(log(abs(fftshift(d2tf))+1)),shading interp,colormap(hot);
-title('Log Frequency Spectrum','fontsize',20), xlabel('Kx'), ylabel('Ky');
+    % Color Image
+    color = imread('moon','jpeg');
+    color_d = double(color);
+    Fcolor = exp(-Width*((Kx-size(bw,2)/2).^2+(Ky-size(bw,1)/2).^2));
+    Fcolor_t = fftshift(Fcolor);
+    color_t = zeros(size(bw,1),size(bw,2),3);
 
-% Color image
-dcolor = imread('moon','jpeg');
-dcolord = double(dcolor);
-width = 0.001;
-Fcolor = exp(-width*((Kx-size(d2,2)/2).^2+(Ky-size(d2,1)/2).^2));
-Fcolort = fftshift(Fcolor);
+    for j=1:3
+        color_t(:,:,j) = fft2(color_d(:,:,j));
+        colort_f(:,:,j) = Fcolor_t.*color_t(:,:,j);    
+        recombined_image(:,:,j) = uint8(abs(ifft2(colort_f(:,:,j))));
+    end
 
-dcolort = zeros(size(d2,1),size(d2,2),3);
+    figure(4), pcolor(log(abs(fftshift(color_t(:,:,1)))+1)),...
+        shading interp, colormap(hot);
+    title('Log Frequency Spectrum','fontsize',20), ...
+        xlabel('Kx'), ylabel('Ky');
 
-for j=1:3
-    dcolort(:,:,j) = fft2(dcolord(:,:,j));
-    dcolortf(:,:,j) = Fcolort.*dcolort(:,:,j);    
-    recombined(:,:,j) = uint8(abs(ifft2(dcolortf(:,:,j))));
-end
+    figure(5)
+    imshow(color), title('Noisy Image','fontsize',20);
 
-figure(5), pcolor(log(abs(fftshift(dcolort(:,:,1)))+1)),shading interp,colormap(hot);
-title('Log Frequency Spectrum','fontsize',20), xlabel('Kx'), ylabel('Ky');
-
-figure(6)
-imshow(dcolor), title('Noisy Image','fontsize',20);
-
-figure(7)
-imshow(recombined), title('Filtered Image','fontsize',20);
+    figure(6)
+    imshow(recombined_image), title('Filtered Image','fontsize',20);
 end
 
 
 %% Local Blurring (Color)
 function Color
-% Image is Color
-color = imread('moon','jpeg');
-colord = double(color);
-[nx,ny] = size(colord(:,:,1));
+    
+    % Image is in Color
+    color = imread('moon','jpeg');
+    color_d = double(color);
+    [nx,ny] = size(color_d(:,:,1));
 
-% Build Tri-Diagonal Matrix
-x = linspace(0,1,nx); 
-y = linspace(0,1,ny); 
-dx=x(2)-x(1); 
-dy=y(2)-y(1);
-onex = ones(nx,1); 
-oney = ones(ny,1);
+    % Build the Tri-Diagonal Matrix
+    x = linspace(0,1,nx); y = linspace(0,1,ny); 
+    dx = x(2)-x(1); dy = y(2)-y(1);
+    onex = ones(nx,1); oney = ones(ny,1);
+    Ix = eye(nx); Iy = eye(ny);
+    
+    Dx = (spdiags([onex, -2*onex, onex],[-1, 0, 1],nx,nx)/dx^2);
+    Dy = (spdiags([oney, -2*oney, oney],[-1, 0, 1],ny,ny)/dy^2);
+    
+    % Find the Laplacian
+    L = kron(Iy,Dx) + kron(Dy,Ix); %Kronecker tensor product
 
-Dx = (spdiags([onex -2*onex onex],[-1 0 1],nx,nx)/dx^2);
-Ix = eye(nx);
+    % Build the Diffusion Coefficients
+    D = zeros(nx,ny);
+    D(1:220,185:280)=1;
+    D = reshape(D,nx*ny,1);
 
-Dy = (spdiags([oney -2*oney oney],[-1 0 1],ny,ny)/dy^2);
-Iy = eye(ny);
-
-% Laplacian
-L = kron(Iy,Dx) + kron(Dy,Ix); %Kronecker tensor product
-
-% Build Diffusion Coefficients
-D = zeros(nx,ny);
-D(1:220,185:280)=1;
-D = reshape(D,nx*ny,1);
-
-% Solve Diffusion Equation
-tspan = [0.00001, 0.00002, 0.0001, 0.001];
-for j=1:3
-    An0 = reshape(colord(:,:,j),nx*ny,1);
-    [t, solution] = ode45('img_rhs',tspan,An0,[],L,D);
-    figure(j+7)
-    for k=1:length(tspan)
-        subplot(2,2,k)
-        diffused(:,:,k) = uint8(reshape(solution(k,:),nx,ny));
-        imshow(diffused(:,:,k));
-        recombined(:,:,j,k) = diffused(:,:,k);
+    % Solve the Diffusion Equation
+    tspan = [0.00001, 0.00002, 0.0001, 0.001];
+    for j=1:3
+        An0 = reshape(color_d(:,:,j),nx*ny,1);
+        [t, solution] = ode45('img_rhs',tspan,An0,[],L,D);
+        figure(j+6)
+        for k=1:length(tspan)
+            subplot(2,2,k)
+            diffused_image(:,:,k) = uint8(reshape(solution(k,:),nx,ny));
+            imshow(diffused_image(:,:,k));
+            recombined_image(:,:,j,k) = diffused_image(:,:,k);
+        end
     end
-end
 
-figure(11)
-for j=1:length(tspan)
-   subplot(2,2,j), imshow(recombined(:,:,:,j)), title(['Diffusion Time:  ' num2str(tspan(j)) 's'],'fontsize',20);
-end
+    figure(10)
+    for i=1:length(tspan)
+       subplot(2,2,i), imshow(recombined_image(:,:,:,i)), ...
+           title(['Diffusion Time: ' num2str(tspan(i)) 's'],'fontsize',20);
+    end
 
-figure(12)
-imshow(color), title('Original Image')
+    figure(11)
+    imshow(color), title('Original Image')
 end
 
 
 %% Local Blurring (Black & White)
 function BW
-% Image is Black/White
-bw = imread('moon_bw','bmp');
-bwd = double(bw);
-[nx,ny] = size(bwd(:,:));
+    
+    % Image is in Black/White
+    bw = imread('moon_bw','bmp');
+    bw_d = double(bw);
+    [nx,ny] = size(bw_d(:,:));
 
-% Build Tri-Diagonal Matrix
-x = linspace(0,1,nx);
-y = linspace(0,1,ny);
-dx=x(2)-x(1);
-dy=y(2)-y(1);
-onex = ones(nx,1);
-oney = ones(ny,1);
+    % Build the Tri-Diagonal Matrix
+    x = linspace(0,1,nx); y = linspace(0,1,ny);
+    dx = x(2)-x(1); dy = y(2)-y(1);
+    onex = ones(nx,1); oney = ones(ny,1);
+    Ix = eye(nx);Iy = eye(ny);
+    
+    Dx = (spdiags([onex, -2*onex, onex],[-1, 0, 1],nx,nx)/dx^2);
+    Dy = (spdiags([oney, -2*oney, oney],[-1, 0, 1],ny,ny)/dy^2);
 
-Dx = (spdiags([onex -2*onex onex],[-1 0 1],nx,nx)/dx^2);
-Ix = eye(nx);
+    % Find the Laplacian
+    L = kron(Iy,Dx) + kron(Dy,Ix); %Kronecker tensor product
 
-Dy = (spdiags([oney -2*oney oney],[-1 0 1],ny,ny)/dy^2);
-Iy = eye(ny);
+    % Build the Diffusion Coefficients
+    D = zeros(nx,ny);
+    D(1:220,185:280)=10;
+    D = reshape(D,nx*ny,1);
 
-% Laplacian
-L = kron(Iy,Dx) + kron(Dy,Ix); %Kronecker tensor product
+    % Solve the Diffusion Equation
+    tspan = [0.00001, 0.00002, 0.0001, 0.001];
+    An0 = reshape(bw_d(:,:),nx*ny,1);
+    [t, solution] = ode45('img_rhs',tspan,An0,[],L,D);
 
-% Build Diffusion Coefficients
-D = zeros(nx,ny);
-D(1:220,185:280)=10;
-D = reshape(D,nx*ny,1);
-
-% Solve Diffusion Equation
-tspan = [0.00001, 0.00002, 0.0001, 0.001];
-An0 = reshape(bwd(:,:),nx*ny,1);
-[t, solution] = ode45('img_rhs',tspan,An0,[],L,D);
-
-figure()
-for k=1:length(tspan)
-    subplot(2,2,k)
-    diffused(:,:,k) = uint8(reshape(solution(k,:),nx,ny));
-    imshow(diffused(:,:,k)); title(['Diffusion Time:  ' num2str(tspan(k)) 's'],'fontsize',20);
-end
+    figure(12)
+    for k=1:length(tspan)
+        subplot(2,2,k)
+        diffused_image(:,:,k) = uint8(reshape(solution(k,:),nx,ny));
+        imshow(diffused_image(:,:,k)); 
+        title(['Diffusion Time: ' num2str(tspan(k)) 's'],'fontsize',20);
+    end
 end
